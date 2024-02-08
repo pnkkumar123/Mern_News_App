72f880644f0559fe5fb85968f8ab0854d9c6e921
pnkkumar123 <pankajkanwar420@gmail.com>
2024-02-08 12:27:04 +0530

    added backend,connected mongodb to backend

---
+++ NewsApp_Backend/api/index.js
@@ -0,0 +1,35 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import router from './routes/user.route.js';
+import { authRouter } from './routes/auth.route.js';
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGODB_URI)
+.then(()=>{
+    console.log('connected to mongoDB');
+})
+.catch((error)=>{
+    console.log(error);
+})
+
+const app = express();
+
+app.use(express.json());
+
+app.listen(3000,()=>{
+    console.log('server listening on port');
+})
+app.use("/api/user",router);
+app.use("/api/auth",authRouter);
+
+app.use((error,res,req,next)=>{
+    const statusCode = error.statusCode || 500;
+    const message = error.message || 'Internal Server Error';
+    return res.status(statusCode).json({
+        sucess:false,
+        message,
+        statusCode,
+    })
+})
---
+++ NewsApp_Backend/api/routes/auth.route.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import User from '../models/user.model.js'
+
+const authRouter = express.Router();
+
+authRouter.post("/signup",async (req,res,next)=>{
+    const {username,email,password} = req.body;
+    const newUser = new User ({username,email,password});
+    try{
+        await newUser.save();
+        res.status(201).json({message:"User created sucessfully"});
+    }catch(e){
+        next(e)
+    }
+})
+
+export {authRouter}
