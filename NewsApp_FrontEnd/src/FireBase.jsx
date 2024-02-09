2efa0d1c91620ae6a9eeec7f4856d0517b4ee43f
pnkkumar123 <pankajkanwar420@gmail.com>
2024-02-09 11:27:07 +0530

    added api of firebase in .env

--- NewsApp_FrontEnd/src/FireBase.jsx
+++ NewsApp_FrontEnd/src/FireBase.jsx
@@ -7,7 +7,7 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDwpSY4E8b-fUlVeDw1OGo7Uvc6-VXr2C4",
+  apiKey: import.meta.env.VITE_API_FIREBASE,
   authDomain: "newsapp-0007.firebaseapp.com",
   projectId: "newsapp-0007",
   storageBucket: "newsapp-0007.appspot.com",
